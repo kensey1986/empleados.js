c2b19e68691e87e28afd31089e603b7fe15c1a9c
Pedro Danjack Ortiz <danjack010892@gmail.com>
2022-04-26 10:28:40 -0500

    first commit

---
+++ empleados.js
@@ -0,0 +1,50 @@
+const empleados = [
+    {
+        id: 1,
+        name: 'Juan',
+        salary: 100,
+        department: "ventas"
+    },
+    {
+        id: 2,
+        name: 'Carlos',
+        salary: 50,
+        department: "servicio al cliente"
+    },
+    {
+        id: 3,
+        name: 'Maria',
+        salary: 75,
+        department: "tecnologia"
+    },
+    {
+        id: 4,
+        name: 'Graciela',
+        salary: 30,
+        department: "ventas"
+    },
+    {
+        id: 5,
+        name: 'Alvaro',
+        salary: 80,
+        department: "administrativo"
+    },
+    {
+        id: 6,
+        name: 'Manuel',
+        salary: 90,
+        department: "administrativo"
+    },
+    {
+        id: 7,
+        name: 'Pedro',
+        salary: 65,
+        department: "servicio al cliente"
+    },
+    {
+        id: 8,
+        name: 'Esteban',
+        salary: 85,
+        department: "tecnologia"
+    },
+];
